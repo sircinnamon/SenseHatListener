4ca650a8e4402c4cea28c39ba45e1fe477971ec3
Riley Lahd <Riley.Lahd@sjrb.ca>
2019-02-22 12:52:59 -0700

    Hide post body behind higher logging level

--- sensehat_listener.py
+++ sensehat_listener.py
@@ -118,8 +118,8 @@ class S(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n",str(self.path), str(self.headers))
+        logging.debug("POST Body:\n%s\n", post_data.decode('utf-8'))
         handle_post_body(post_data.decode('utf-8'))
 
         self._set_response()
