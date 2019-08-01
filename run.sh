c6e6058625fe6af658eb6cfa67625e6067e87e1a
Riley Lahd <Riley.Lahd@sjrb.ca>
2019-08-01 14:44:54 -0600

    Add flags to runscript

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ docker run -d \
 	--name $PYTHON_CONTAINER \
 	--net $NETWORK_NAME --ip $PYTHON_IP \
 	--privileged \
-	sensehat python3 /sensehat_listener.py 80
+	sensehat python3 /sensehat_listener.py -p 80
 
 docker rm $NGINX_CONTAINER
 docker run -d \
